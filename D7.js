b11df9b1834a09b26fa743c7ca16fca5e997cfa7
Billions94 <E.a.egbedi@gmail.com>
2021-09-21 17:03:09 +0200

    commit

--- D7.js
+++ D7.js
@@ -1,8 +1,78 @@
+let nameArray = [];
+let userNameArray = [];
+let userEmailArray = [];
+
+const infoList = (info) => `
+<li class="list-group-item">${info}</li>`
+
 const fetchUserData = async() => {
     const response = await  fetch(`https://jsonplaceholder.typicode.com/users`)
     const userData = await response.json()
     console.log(userData)
+
+    return userData
 }
+
+let name = document.getElementById('name')
+let username = document.getElementById('username')
+let email = document.getElementById('email')
+let userInfo = document.getElementById('userInfo')
+
+const showNames = () => {
+
+    fetchUserData()
+    .then(data => {
+        console.log(data)
+        userData.innerHTML = ""
+        
+        data.forEach(name => {
+            console.log(name)
+            userData.innerHTML += infoList(name.name)
+            nameArray.push(name.name)
+        });
+
+    })
+}
+
+
+const showUserName = () => {
+    fetchUserData()
+    .then(data => {
+        console.log(data)
+        userData.innerHTML = ""
+
+        data.forEach(username => {
+            console.log(username)
+            userData.innerHTML += infoList(username.username)
+            userNameArray.push(username.username)
+        });
+
+    })
+}
+
+const showEmail = () => {
+    fetchUserData()
+    .then(data => {
+        console.log(data)
+        userData.innerHTML = ""
+
+        data.forEach(email => {
+            console.log(email)
+            userData.innerHTML += infoList(email.email)
+            userEmailArray.push(email.email)
+        });
+
+    })
+}
+const searchUsers = () => {
+    const searchUser = document.querySelector("input[type=search]").value
+    console.log(searchUser)
+
+    
+}
+
+
+
 
 
 
@@ -10,4 +80,5 @@ const fetchUserData = async() => {
 
 window.onload = () => {
     fetchUserData()
+    // showNames()
 }
